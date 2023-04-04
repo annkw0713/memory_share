5af0d8892256e35e49534ae05fa6d944961e2d0b
annkw0713 <annkw75239549@gmail.com>
2023-04-04 15:03:50 +0900

    複数枚画像を投稿する機能の実装

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,6 +1,7 @@
 class Memory < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
 
-  validates :title, :date, :person, :place, :description, :image, presence: true
+  validates :title, :date, :person, :place, :description, :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 20, message: "は1枚以上20枚以下にしてください" }
 end
