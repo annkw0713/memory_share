f4d3b8cb627b0fee402c1216ee911a19af3b320d
annkw0713 <annkw75239549@gmail.com>
2023-04-06 14:34:00 +0900

    movie_uploader.rbの修正と細かな修正

--- app/uploaders/movie_uploader.rb
+++ app/uploaders/movie_uploader.rb
@@ -4,8 +4,8 @@ class MovieUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file if Rails.env.test?
-  storage :fog if Rails.env.production?
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
